a5b648c9d89a9c549b07d0f728cee8af4ff2241b
NadnerbD <brendandorr@gmail.com>
2012-01-28 16:27:18 -0500

    Changed Launcher.py to only import neccessary modules

--- Python/Launcher.py
+++ Python/Launcher.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python
-
-from CC_Server import CC_Server
-from CC_Relay import CC_Relay
-from TIA_Server import TIA_Server
-from CC_Skype_Relay import CC_Skype_Relay
-from Term_Server import Term_Server
-
 import sys
 
 serverClasses = { \
-	"cc": CC_Server, \
-	"tia": TIA_Server, \
-	"relay": CC_Relay, \
-	"skype": CC_Skype_Relay, \
-	"term": Term_Server, \
+	"cc": "CC_Server", \
+	"tia": "TIA_Server", \
+	"relay": "CC_Relay", \
+	"skype": "CC_Skype_Relay", \
+	"term": "Term_Server", \
 }
 serverClass = None
-for arg in sys.argv:
-	if(serverClasses.has_key(arg)):
-		serverClass = serverClasses[arg]
+if(len(sys.argv) == 2 and serverClasses.has_key(sys.argv[1])):
+	serverName = serverClasses[sys.argv[1]]
+	serverModule = __import__(serverName)
+	serverClass = serverModule.__getattribute__(serverName)
 if(serverClass):
 	Server = serverClass()
 	Server.readPrefs("etc/CCServer.conf")
