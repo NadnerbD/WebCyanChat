5866ba246163463f96fafcca5fca5ab8945ce4ce
NadnerbD <brendandorr@gmail.com>
2016-01-22 11:13:23 -0500

    Added command line preference overrides to Launcher

--- Python/Launcher.py
+++ Python/Launcher.py
@@ -14,13 +14,22 @@ serverClasses = { \
 	"cursive": "Cursive_Server", \
 }
 serverClass = None
-if(len(sys.argv) == 2 and serverClasses.has_key(sys.argv[1])):
+if(len(sys.argv) >= 2 and serverClasses.has_key(sys.argv[1])):
 	serverName = serverClasses[sys.argv[1]]
 	serverModule = __import__(serverName)
 	serverClass = serverModule.__getattribute__(serverName)
-if(serverClass):
+try:
 	Server = serverClass()
 	Server.readPrefs("etc/CCServer.conf")
+	# prefs can be overridden with command line options
+	for arg in sys.argv[2:]:
+		(name, value) = arg.split("=", 1)
+		if(name.startswith("--") and value):
+			if(value.isdigit()):
+				value = int(value)
+			Server.prefs[name[2:]] = value
+		else:
+			raise Exception("invalid option")
 	Server.start()
-else:
-	print "usage: python %s [%s]" % (sys.argv[0], '|'.join(serverClasses.keys()))
+except Exception:
+	print "usage: python %s (%s) [--pref_name=VALUE ...]" % (sys.argv[0], '|'.join(serverClasses.keys()))
