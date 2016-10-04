4711a4f3c713c72564800693464c90decb6dc4ad
NadnerbD <brendandorr@gmail.com>
2016-10-04 01:30:47 +0000

    Improve heroku shell stuff

--- vim-on-heroku.sh
+++ vim-on-heroku.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 curl https://s3.amazonaws.com/heroku-jvm-buildpack-vi/vim-7.3.tar.gz --output vim.tar.gz
-mkdir vim && tar xzvf vim.tar.gz -C vim
+mkdir vim && tar xzf vim.tar.gz -C vim
 export PATH=$PATH:/app/vim/bin
 export VIM=/app/vim/share/vim/vim73
+alias vi=vim
+alias ls="ls --color=always"
+git config --global core.editor vim
