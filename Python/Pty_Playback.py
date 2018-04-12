1dff6653c31613a8eb04cbfa249346a2d512427d
NadnerbD <brendandorr@gmail.com>
2018-04-11 20:52:45 -0400

    Added program to play back files produced by Tee_Pty

---
+++ Python/Pty_Playback.py
@@ -0,0 +1,34 @@
+import sys, termios, tty
+from StringIO import *
+
+def readToEsc(stream):
+	sio = StringIO()
+	while True:
+		c = stream.read(1)
+		if c == '\033':
+			return sio.getvalue()
+		elif c == '':
+			raise Exception
+		else:
+			sio.write(c)
+
+if len(sys.argv) == 2:
+	attrs = termios.tcgetattr(sys.stdin)
+	tty.setraw(sys.stdin.fileno())
+	inFile = open(sys.argv[1], 'r')
+	while True:
+		try:
+			so = readToEsc(inFile)
+		except:
+			termios.tcsetattr(sys.stdin, termios.TCSADRAIN, attrs)
+			print "\r\n\033[31mREACHED END OF OUTPUT\033[m"
+			break
+		sys.stdout.write(so.decode('string-escape'))
+		sys.stdout.flush()
+		# skip the user input
+		readToEsc(inFile)
+		inFile.read(2)
+		sys.stdin.read(1)
+	inFile.close()
+else:
+	print 'usage: %s <log_file>' % sys.argv[0]
