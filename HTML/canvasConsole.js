c6c86e748eed66cb271927b231552794f79c05a3
NadnerbD <brendandorr@gmail.com>
2018-05-10 13:30:50 -0400

    Updated canvasConsole to be resizable

--- HTML/canvasConsole.js
+++ HTML/canvasConsole.js
@@ -21,22 +21,35 @@ function setTheme(theme) {
 }
 
 function sizeToWindow() {
+	var charRect = document.querySelector("span#char").getBoundingClientRect();
+	reqResize(
+		Math.floor(window.innerWidth / charRect.width),
+		Math.floor(window.innerHeight / charRect.height)
+	);
 }
 
 function startDisplay() {
-	var canvas = document.querySelector("canvas");
-	// fill loop
+	var canvas = document.querySelector("canvas#grid");
 	var ctx = canvas.getContext("2d");
-	var width = canvas.width;
-	var height = canvas.height;
-	ctx.textBaseline = "top";
-	var ch = 24;
-	ctx.font = ch + "px monospace";
-	var cw = ctx.measureText("0").width;
+	var span = document.querySelector("span#char").getBoundingClientRect();
+	var ch = span.height;
+	var cw = span.width;
+	function canvasResize(event) {
+		canvas.height = window.innerHeight;
+		canvas.width = window.innerWidth;
+		ctx = canvas.getContext("2d");
+		ctx.textBaseline = "hanging";
+		ctx.font = ch + "px monospace";
+		ctx.fillStyle = "rgba(0, 0, 0, 1.0)";
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+	}
+	window.addEventListener("resize", canvasResize);
+	canvasResize();
+	// fill loop
 	setInterval(function() {
 		// instead of clearing fade
 		ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
-		ctx.fillRect(0, 0, width, height);
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
 		// then add our data
 		ctx.fillStyle = "rgb(0, 255, 0)"
 		for(var y = 0; y < grid.height; y++) {
@@ -44,11 +57,11 @@ function startDisplay() {
 			ctx.fillText(line, 0, ch * y);
 		}
 		// cursor
-		var cursorx = grid.cursor % grid.width;
-		var cursory = Math.floor(grid.cursor / grid.width);
-		ctx.fillRect(cursorx * cw, cursory * ch, cw, ch);
-		// cursor contains inverted copy of the letter it is occluding
 		if(grid.cursor != -1) {
+			var cursorx = grid.cursor % grid.width;
+			var cursory = Math.floor(grid.cursor / grid.width);
+			ctx.fillRect(cursorx * cw, cursory * ch, cw, ch);
+			// cursor contains inverted copy of the letter it is occluding
 			ctx.fillStyle = "rgb(0, 0, 0)";
 			ctx.fillText(grid.cells[grid.cursor].glyph, cursorx * cw, cursory * ch);
 		}
